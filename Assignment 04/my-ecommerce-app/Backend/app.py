c871b242e00aa58ee863b695e9d3a52509bf1a77
jin kim <jinkim477@gmail.com>
2024-03-28 17:03:47 -0600

    added py stuff

--- Assignment 04/my-ecommerce-app/Backend/app.py
+++ Assignment 04/my-ecommerce-app/Backend/app.py
@@ -6,3 +6,101 @@ import os
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route('/login', methods=['POST'])
+def register_user():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    email = request.form.get("email")
+    
+    for user in users:
+        if user["username"] == username:
+            return ('', 404) # Display an error message in the frontend
+        else:
+            users.append({
+                "username": username,
+                "password": password,
+                "email": email
+            })
+            return 201
+    return ('', 400)
+
+@app.route('/login', methods=['POST'])
+def sign_in():
+    
+    return
+
+users = []
+
+products = [
+    {
+        "id": 1,
+        "name": "Product 1",
+        "description": "Description for Product 1",
+        "price": 10.99,
+        "image": 'images/product1.png'
+    },
+    {
+        "id": 2,
+        "name": "Product 2",
+        "description": "Description for Product 2",
+        "price": 20.99,
+        "image": 'images/product2.jpg'
+    },
+    {
+        "id": 3,
+        "name": "Product 3",
+        "description": "Description for Product 3",
+        "price": 10.99,
+        "image": 'images/product3.jpg'
+    },
+    {
+        "id": 4,
+        "name": "Product 4",
+        "description": "Description for Product 4",
+        "price": 10.99,
+        "image": 'images/product4.jpg'
+    },
+    {
+        "id": 5,
+        "name": "Product 5",
+        "description": "Description for Product 5",
+        "price": 10.99,
+        "image": 'images/product5.jpg'
+    },
+    {
+        "id": 6,
+        "name": "Product 6",
+        "description": "Description for Product 6",
+        "price": 10.99,
+        "image": 'images/product6.jpg'
+    },
+    {
+        "id": 7,
+        "name": "Product 7",
+        "description": "Description for Product 7",
+        "price": 10.99,
+        "image": 'images/product7.jpg'
+    },
+    {
+        "id": 8,
+        "name": "Product 8",
+        "description": "Description for Product 8",
+        "price": 10.99,
+        "image": 'images/product8.jpg'
+    },
+    {
+        "id": 9,
+        "name": "Product 9",
+        "description": "Description for Product 9",
+        "price": 10.99,
+        "image": 'images/product9.jpg'
+    },
+    {
+        "id": 10,
+        "name": "Product 10",
+        "description": "Description for Product 10",
+        "price": 10.99,
+        "image": 'images/product10.jpg'
+    }
+]
